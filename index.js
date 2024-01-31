aedd00a3f23e9b673204911c07417fda31ea12a8
LopezFarm <60264690+Yafet4275@users.noreply.github.com>
2024-01-30 21:56:28 -0600

    Update index,js
    Removed passport.authenticate from movie endpoint

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ userSchema.methods.validatePassword = function(password) {
   return bcrypt.compareSync(password, this.Password);
 };
 
-app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
+app.get('/movies', (req, res) => {
   Movie.find()
     .then((movie) => {
       res.status(200).json(movie);
